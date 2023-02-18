da9dd0a2dbf349bcdf7715891d3982956f553aaf
hoanganh8815 <nhanh36@gmail.com>
2023-02-18 12:11:35 +0700

    1 file modified, 4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { NameEditorComponent } from './name-editor/name-editor.component';
 import { ProfileEditorComponent } from './profile-editor/profile-editor.component';
+import { EmailInputComponent } from './email-input/email-input.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NameEditorComponent,
-    ProfileEditorComponent
+    ProfileEditorComponent,
+    EmailInputComponent
   ],
   imports: [
     BrowserModule,
