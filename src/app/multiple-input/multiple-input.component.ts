fdcd5ec16a265cc575fcf2e38bc2e7fc82fc8b54
hoanganh8815 <nhanh36@gmail.com>
2023-02-19 22:29:40 +0700

    3 files modified

--- src/app/multiple-input/multiple-input.component.ts
+++ src/app/multiple-input/multiple-input.component.ts
@@ -4,12 +4,21 @@ import { FormControl, FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-multiple-input',
   templateUrl: './multiple-input.component.html',
-  styleUrls: ['./multiple-input.component.css']
+  styleUrls: ['./multiple-input.component.css'],
 })
 export class MultipleInputComponent {
-
   multiFormGroup = new FormGroup({
-    firstName : new FormControl(''),
-    lastName : new FormControl('')
-  })
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    address: new FormGroup({
+      street: new FormControl(''),
+      city: new FormControl(''),
+      state: new FormControl(''),
+      zip: new FormControl(''),
+    }),
+  });
+
+  onSubmit() {
+    console.warn(this.multiFormGroup.value);
+  }
 }
